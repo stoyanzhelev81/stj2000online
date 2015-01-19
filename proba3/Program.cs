02b868eccdfb380d8e49de987da1fd5bce079c84
Stoyan Zhelev <stoyanzhelev81@gmail.com>
2015-01-19 18:27:51 +0200

    .init

---
+++ proba3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PrintSequence
+{
+    static void Main()
+    {
+        Console.Write("Enter your birth date in format dd.mm.yyyy:");
+        Console.Read();
+        
+    }
+}
