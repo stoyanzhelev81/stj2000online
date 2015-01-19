d7083605d4cf20acd05c6b3a0299dd68fc67dd6a
Stoyan Zhelev <stoyanzhelev81@gmail.com>
2015-01-19 21:08:20 +0200

    .init

---
+++ PrimitiveDataTypesAndVariables/13.ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    class ComparingFloats
+    {
+        static void Main()
+        // Problem 13.* Comparing Floats
+        // Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001.
+        {
+            Console.WriteLine("Enter first number to compare");
+            double first = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number to compare");
+            double second = double.Parse(Console.ReadLine());
+
+            double compare = Math.Abs(first - second);
+            double eps = 0.000001;
+
+            if (compare <= eps)
+
+                Console.WriteLine("Equal");
+            
+            else
+
+                Console.WriteLine("Not equal");
+            
+        }
+    }
+
---
+++ PrimitiveDataTypesAndVariables/14.PrintTheASCIITable/PrintTheASCIITable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class PrintTheASCIITable
+    {
+        static void Main()
+        // Problem 14.* Print the ASCII Table
+        // Find online more information about ASCII (American Standard Code for Information Interchange) and write a program that prints the entire ASCII table of characters on the console (characters from 0 to 255).
+        {
+            for (int i = 0; i < 255; i++)
+            Console.WriteLine((char)i);  
+        }
+    }
+
